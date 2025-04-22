e6ebf0ca46de537c6178b3d4d488f28737195074
metacomputers <info.metacomputers@gmail.com>
2025-04-22 22:26:06 +0530

    22-04

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,6 +1,6 @@
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
-import Header from './components/HeaderFooter/Header';
-import Footer from './components/HeaderFooter/Footer';
+import Header from './components/common/Header';
+import Footer from './components/common/Footer';
 import HomePage from './pages/Landing/HomePage';
 import CartPage from './pages/Cart/CartPage';
 import UserOrdersPage from './pages/Orders/UserOrdersPage';
@@ -9,7 +9,7 @@ function App() {
   return (
     <Router>
       <Header />
-      <main className="min-h-screen pt-24"> {/* Added padding-top for fixed header */}
+      <main className="min-h-screen pt-24"> 
         <Routes>
           <Route path="/cart" element={<CartPage />} />
           <Route path="user/orders" element={<UserOrdersPage />} />
