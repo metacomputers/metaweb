d1d9f747b304dac524671311e814029ee49647f2
Tharindu Mishen <tharindumishen7@gmail.com>
2025-04-29 21:58:02 +0530

    PDF updated

--- frontend/src/components/product_management/StockPDF.jsx
+++ frontend/src/components/product_management/StockPDF.jsx
@@ -56,7 +56,7 @@ const StockPDF = ({ allProducts }) => {
             pdf.text("Brand", 75, y + 5);
             pdf.text("Category", 105, y + 5);
             pdf.text("Quantity", 135, y + 5);
-            // pdf.text("Price (LKR)", 160, y + 5);
+            pdf.text("Unit Price (LKR)", 160, y + 5);
 
             //Table content
             y += 10;
@@ -75,7 +75,7 @@ const StockPDF = ({ allProducts }) => {
                     pdf.text("Brand", 75, y + 5);
                     pdf.text("Category", 105, y + 5);
                     pdf.text("Quantity", 135, y + 5);
-                    // pdf.text("Price (LKR)", 160, y + 5);
+                    pdf.text("Unit Price (LKR)", 160, y + 5);
 
                     y += 10;
                 }
@@ -97,7 +97,7 @@ const StockPDF = ({ allProducts }) => {
                 pdf.text(product.brand || "N/A", 75, y);
                 pdf.text(product.category || "N/A", 105, y);
                 pdf.text(product.quantity?.toString() || "0", 135, y);
-                // pdf.text(product.price?.toLocaleString() || "0", 160, y);
+                pdf.text(product.price?.toLocaleString() || "0", 160, y);
 
                 y += 7;
             });
@@ -139,13 +139,13 @@ const StockPDF = ({ allProducts }) => {
     };
 
     return (
-        <button 
+        <button
             className={`bg-blue-600 text-white py-2 px-4 rounded-md hover:bg-blue-700 transition flex items-center ${isGenerating ? 'opacity-70 cursor-not-allowed' : ''}`}
-            onClick = {generateProductPDF}
-            disabled = {isGenerating}
+            onClick={generateProductPDF}
+            disabled={isGenerating}
         >
 
-            <FaFileDownload className="mr-2"/>
+            <FaFileDownload className="mr-2" />
             {isGenerating ? 'Generating PDF...' : 'Download Product List'}
         </button >
     );
